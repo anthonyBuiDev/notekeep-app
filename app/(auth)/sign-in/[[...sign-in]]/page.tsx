3b5c5656549e8ca7f908d526cc27d596d6df7093
anthonyBuiDev <thanhnambui201@gmail.com>
2024-05-15 16:38:11 +0700

    add after sign up

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="m-12 flex items-center justify-center">
-      <SignIn />
+      <SignIn fallbackRedirectUrl="/notes" />
     </div>
   );
 }
